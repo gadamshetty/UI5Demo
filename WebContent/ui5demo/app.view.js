153b36275e5fabcceeba91f0e3cba006462d3591
gadamshetty@gmail.com <gadamshetty@gmail.com>
2015-03-17 17:27:59 -0500

    Initial Commit
    Initial Commit

---
+++ WebContent/ui5demo/app.view.js
@@ -0,0 +1,40 @@
+sap.ui.jsview("ui5demo.app", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf ui5demo.app
+	*/ 
+	getControllerName : function() {
+		return "ui5demo.app";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf ui5demo.app
+	*/ 
+	createContent : function(oController) {
+		
+		var customerOrders = new sap.m.StandardTile('customerordertile',{
+			title:"Customer Orders Demo",
+			type: sap.m.StandardTileType.None,
+			info:"customer orders",
+			icon:"sap-icon://plus",
+			press: oController.openCustOrders
+		});
+		
+		var tileContainer = new sap.m.TileContainer('tilecontainer',{
+			editable: false
+		});
+		
+		tileContainer.addTile(customerOrders);
+		
+ 		return new sap.m.Page({
+			title: "Title",
+			enableScrolling: false,
+			content: [
+			          	tileContainer
+			]
+		});
+	}
+
+});
